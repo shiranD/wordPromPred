2698d58944f33e534a4ffb5d889987f97ddeca39
Shiran <dudy@ohsu.edu>
2015-03-06 10:33:40 -0800

    max bug

--- mp4/checks.py
+++ mp4/checks.py
@@ -5,26 +5,60 @@ import bitweight
 from numpy import log2
 
 X_train, y_train, X_test, y_test = data_prep()  
-trans = MaximumLikelihoodNGramModel(y_train,3).prob.items() # transitions 
+trans = MaximumLikelihoodNGramModel(y_train,1).prob.items() # transitions 
 trans = dict(trans)
 
-#for (t_2,t_1),a in trans.iterkeys():
-ent = 0
-all_dist=[]
-for prefix in trans.iterkeys():
-    (p2,p1)=prefix
-    if p2=="<S_1>" or p1=="<S_1>" or p2=="<S_0>" or p1=="<S_0>":
-        continue
-    for suffix in ["0","1","2"]:#,"<S_1>","</S_1>"
-        if trans[prefix][suffix].to_real!=0:
-            #print "transition from ",prefix, "prefix to ",suffix," is ", trans[prefix][suffix]
-            a = trans[prefix][suffix].to_real
-            d = (a,prefix,suffix)
-            all_dist.append(d)
-            
-            ent+= a*log2(a)
+if 1:
+    ent = 0
+    tran=dict(trans)
+    for state in ["0","1","2"]:
+        print tran[()][state].to_real
+        a=tran[()][state].to_real
+        ent +=a*log2(a)
+    print "tag entropy per symbol is ", ent
     
-print "the entropy is", ent
-print "the entropy for trinomial distribution is ", log2(0.05)#1/18
+trans = MaximumLikelihoodNGramModel(y_train,3).prob.items() # transitions 
+trans = dict(trans)
+if 1:
+    ent = 0
+    all_dist=[]
+    for prefix in trans.iterkeys():
+        (p2,p1)=prefix
+        if p2=="<S_1>" or p1=="<S_1>" or p2=="<S_0>" or p1=="<S_0>" or p1=='</S_1>' or p2=='</S_1>':
+            continue
+        a = 0
+        ent = 0
+        for suffix in ["0","1","2"]:#,"<S_1>","</S_1>"
+            if trans[prefix][suffix].to_real!=0:
+    
+                a = trans[prefix][suffix].to_real
+                d = (prefix,suffix,a)
+                all_dist.append(d)
+                
+                ent+= a*log2(a)
+        print "the entropy for prefix ",prefix, " is ", ent
+    
+    #print a
+if 0:
+    ent = 0
+    all_dist=[]
+    for suffix in ["0","1","2"]:#,"<S_1>","</S_1>"   
+        a = 0
+        ent = 0
+        for prefix in trans.iterkeys():
+            (p2,p1)=prefix
+            if p2=="<S_1>" or p1=="<S_1>" or p2=="<S_0>" or p1=="<S_0>" or p1=='</S_1>' or p2=='</S_1>':
+                continue        
+            if trans[prefix][suffix].to_real!=0:
+    
+                a = trans[prefix][suffix].to_real
+                d = (prefix,suffix,a)
+                all_dist.append(d)
+                
+                ent+= a*log2(a)
+        print "the entropy for suffix ",suffix, " is ", ent
+            
+print "the entropy for prefix ",prefix, " is ", ent
+print "the entropy for trinomial distribution is ", -log2(3)#1/18
 for item in reversed(sorted(all_dist)):
     print item
--- mp4/viterbi_Prtagger_cls.py
+++ mp4/viterbi_Prtagger_cls.py
@@ -23,42 +23,43 @@ DIGIT = "*DIGIT*"
 
 PUNCTUATION = frozenset(punctuation)
 NUMBER_WORDS = frozenset("""
-zero one two three four five six seven eight nine ten eleven twelve 
-thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty 
-thirty fourty fifty sixty seventy eighty ninety hundred thousand million 
+zero one two three four five six seven eight nine ten eleven twelve
+thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty
+thirty fourty fifty sixty seventy eighty ninety hundred thousand million
 billion trillion quadrillion
 """.upper().split())
 
+
 def extract(History):
-    
+
     if not History:
         return []
-    if len(History)==1:
+    if len(History) == 1:
         return History[0]
     else:
         return History[-1]
 
-    
-def shuffle_set(x,y):
-    
+
+def shuffle_set(x, y):
+
     index = sample(range(len(x)), len(x))
     y_new = []
     x_new = []
     for i in xrange(len(x)):
         y_new.append(y[index[i]])
-        x_new.append(x[index[i]]) 
-        
+        x_new.append(x[index[i]])
+
     return x_new, y_new
-    
-    
+
+
 def tfeats(partial_yyhat, order):
     """
-    Given a list of hypothesized preceding labels `partial_yyhat` and 
-    a Markov `order`, generate the transition features for the next 
+    Given a list of hypothesized preceding labels `partial_yyhat` and
+    a Markov `order`, generate the transition features for the next
     element in the sequence. The `order` parameter determines how far
-    back the features may reach. Let the _history_ be the `order` 
-    preceding tags in `partial_yyhat`, truncated when the `order` is 
-    larger than `partial_yyhat` (i.e., near the left edge). There are two 
+    back the features may reach. Let the _history_ be the `order`
+    preceding tags in `partial_yyhat`, truncated when the `order` is
+    larger than `partial_yyhat` (i.e., near the left edge). There are two
     types of transition tags extracted from the history:
 
     * a "unigram" tag for each tag in the history
@@ -75,7 +76,8 @@ def tfeats(partial_yyhat, order):
     >>> tfeats(partial_yyhat, 2)
     ["t_i-1='MD'", "t_i-2=','", "t_i-2=',',t_i-1='MD'"]
     >>> tfeats(partial_yyhat, 3)
-    ["t_i-1='MD'", "t_i-2=','", "t_i-3='JJ'", "t_i-2=',',t_i-1='MD'", "t_i-3='JJ',t_i-2=',',t_i-1='MD'"]
+    ["t_i-1='MD'", "t_i-2=','", "t_i-3='JJ'", "t_i-2=',',t_i-1='MD'",
+    "t_i-3='JJ',t_i-2=',',t_i-1='MD'"]
 
     History is smaller than the order, but no index-out-of-bounds errors:
 
@@ -83,63 +85,53 @@ def tfeats(partial_yyhat, order):
     >>> tfeats(partial_yyhat, 2)
     ["t_i-1='NNP'"]
     """
-    if order==0 or len(partial_yyhat)==0:
+    if order == 0 or len(partial_yyhat) == 0:
         return []
 
     t_list = []
     order = min(order, len(partial_yyhat))
     for i in xrange(order):
-        t_list.append("t_i-"+str(i+1)+"='"+partial_yyhat[-i-1]+"'")
-        
+        t_list.append("t_i-" + str(i + 1) + "='" + partial_yyhat[-i - 1] + "'")
+
     tt_list = t_list[::-1]
-            
-    for i in xrange(order-1):
-        combo = tt_list[-2-i:]
+
+    for i in xrange(order - 1):
+        combo = tt_list[-2 - i:]
         st = ""
         for st1 in combo:
-            st+=st1+","
+            st += st1 + ","
         t_list.append(st[:-1])
-        
+
     return t_list
+
 
 class AveragedPerceptronTagger(object):
 
     def __init__(self, order, default=None, seed=None):
         self.order = order
-        self.classes = {default} 
-        #self.classes = {"0","1","2"}        
-        
+        self.classes = {default}
         self.random = Random(seed)
         self.weights = defaultdict(partial(defaultdict, LazyWeight))
         self.time = 0
 
     # low-level perceptron operations
-    
+
     def score(self, phi, yhat):
         """
         Get scores for a particular class according to the feature vector `phi`
         """
         return sum(self.weights[phi_i][yhat].get() for phi_i in phi)
-  
 
     def scores(self, phi):
         """
         Get scores for all classes according to the feature vector `phi`
         """
         scores = dict.fromkeys(self.classes, 0)
-        #print scores
+        # print scores
         for phi_i in phi:
             for (cls, weight) in self.weights[phi_i].iteritems():
                 scores[cls] += weight.get()
         return scores
-
-    def predict(self, phi):
-        """
-        Predict most likely class for the feature vector `phi`
-        """
-        scores = self.scores(phi)
-        (yhat, score) = max(scores.iteritems(), key=itemgetter(1))
-        return yhat, score # has max score
 
     def update(self, y, yhat, phi, alpha=1):
         """
@@ -167,23 +159,22 @@ class AveragedPerceptronTagger(object):
 
     def tag_with_features(self, efeats):
         """
-        Tag a list of tokens using a greedy approximation of an HMM, 
+        Tag a list of tokens using a greedy approximation of an HMM,
         using Viterbi returning a (list of tags, list of list of features)
-        tuple; use the `tag` instance method if you want to ignore the features 
+        tuple; use the `tag` instance method if you want to ignore the features
         returned
         """
 
         # build array of dicts
         state_dicts = []
-        for e_phi in efeats: 
+        for e_phi in efeats:
             state_dicts = self.viterbi(e_phi, state_dicts)
-            
-            
+
         # trace back
         yyhat, phis = self.traceback(efeats, state_dicts)
-        assert len(efeats)==len(yyhat)
+        assert len(efeats) == len(yyhat)
 
-        return (yyhat, phis)    
+        return (yyhat, phis)
 
     def tag(self, tokens):
         """
@@ -194,14 +185,14 @@ class AveragedPerceptronTagger(object):
 
     def fit(self, feats, tag_set, epochs, alpha=1):
         """
-        Fit the tagger model 
+        Fit the tagger model
         """
-        classes = ["0","1","2"]
+        classes = ["0", "1", "2"]
         self.register_classes(classes)
         # make an (evaluated) copy of the data, to shuffle it in place
         for i in xrange(1, 1 + epochs):
-            #print(i)
-            feats, tag_set = shuffle_set(feats, tag_set)            
+            # print(i)
+            feats, tag_set = shuffle_set(feats, tag_set)
             for (tokens, tags) in zip(feats, tag_set):
                 self.fit_one(tokens, tags)
         self.finalize()
@@ -220,115 +211,109 @@ class AveragedPerceptronTagger(object):
         Tag held-out labeled corpus `tagged_corpus`, and return tagging
         accuracy
         """
-        corect=0
-        incorect=0        
+        corect = 0
+        incorect = 0
         for (tokens, tags) in zip(feats, tag_set):
-            yyhat= self.tag(tokens)
+            yyhat = self.tag(tokens)
             for pre, tag in zip(yyhat, tags):
-                if pre==tag:
-                    corect+=1
+                if pre == tag:
+                    corect += 1
                 else:
-                    incorect+=1
+                    incorect += 1
 
-        return corect/(corect+incorect), self.weights
-                
+        return corect / (corect + incorect), self.weights
+
     def viterbi(self, e_phi, states_dict):
         """compute the current states scores
-        basing on the previous score, previous states 
+        basing on the previous score, previous states
         and the current emission state score. save history of 2 past
         states and score of previous one that its 'partial score' was the
         highest. The max score is partial score and efeats score."""
-        
-        states = ["0","1","2"]
-        
+
+        states = ["0", "1", "2"]
+
         if not states_dict:
-            first_dict = {}        
-            for state in states:   
-                S_e = self.score(e_phi, state)                 
-                first_dict[state] = (S_e,([]))
-            return [first_dict] 
-        
+            first_dict = {}
+            for state in states:
+                S_e = self.score(e_phi, state)
+                first_dict[state] = (S_e, ([]))
+            return [first_dict]
+
         else:
             last_dict = states_dict[-1]
             this_dict = {}
             scores = self.scores(e_phi)
             for (state, S_e) in scores.iteritems():
-                max_score=-float('inf')
-                max_label = None
+                S_max = -float('inf')
                 for prev in states:
-                    (Sprev, (Hprev))=last_dict[prev]
+                    (Sprev, (Hprev)) = last_dict[prev]
                     if not Hprev:
-                        Hstate = [prev] # no history
+                        Hstate = [prev]  # no history
                     else:
-                        #print Hprev
-                        Hstate = Hprev[-1:]+[prev]# have two states
-                        #print Hstate
+                        # print Hprev
+                        Hstate = Hprev[-1:] + [prev]  # have two states
+                        # print Hstate
                     t_phi = tfeats(Hstate, self.order)
-                                             
-                    partial_score = Sprev+self.score(t_phi, state)
-                    if max_score < partial_score:
+                    partial_score = Sprev + self.score(t_phi, state)
+                    if S_max < partial_score:
                         S_max = partial_score
                         max_hstate = Hstate
                 # write to dict
-                this_dict[state]=(S_max+S_e,(max_hstate))# brakets
+                this_dict[state] = (S_max + S_e, (max_hstate))  # brakets
             states_dict.append(this_dict)
             return states_dict
-        
-    
+
     def traceback(self, efeats, states_dict):
         """return the tag sequence and its
         corresponding phis """
-        
-        states = ["0","1","2"]
+
+        states = ["0", "1", "2"]
         yyhat = []
         phis = []
-        
-        if len(states_dict)==1:
+
+        if len(states_dict) == 1:
             last_dict = states_dict[0]
             last_efeat = efeats[0]
-            
+
         else:
             last_dict = states_dict[-1]
             last_efeat = efeats[-1]
-            
-            
-        (state,(_, Hstate))=max(last_dict.iteritems(),key=itemgetter(1)) 
-        
-        yyhat.append(state)
-        phi = last_efeat+tfeats(Hstate, self.order)
-        phis.append(phi)
-        
-        for (last_dict, e_phi) in zip(reversed(states_dict[:-1]),reversed(efeats[:-1])):
-            prev_state=extract(Hstate)
 
-            (_,Hstate)=last_dict[prev_state]# if H=["2"]->["2"] need "2". H[-1:]
-            #if len(Hstate)==2: # no trigram!
-             #   print "2"            
+        (state, (_, Hstate)) = max(last_dict.iteritems(), key=itemgetter(1))
+
+        yyhat.append(state)
+        phi = last_efeat + tfeats(Hstate, self.order)
+        phis.append(phi)
+
+        for (last_dict, e_phi) in zip(reversed(states_dict[:-1]),
+                                      reversed(efeats[:-1])):
+            prev_state = extract(Hstate)
+
+            # if H=["2"]->["2"] need "2". H[-1:]
+            (_, Hstate) = last_dict[prev_state]
             yyhat.append(prev_state)
-            phi = e_phi+tfeats(Hstate, self.order)
-            phis.append(phi)                
-                         
-        
+            phi = e_phi + tfeats(Hstate, self.order)
+            phis.append(phi)
+
         return yyhat[::-1], phis[::-1]
-        
+
     def register_classes(self, claslist):
         """register classes"""
-        
+
         for cls in claslist:
             self.classes.add(cls)
-            
-if __name__ == "__main__":
-    
 
-    X_train, y_train, X_test, y_test = data_prep()  
-    trans = MaximumLikelihoodNGramModel(y_train,3).prob.items() # transitions 
-    
-    tagger = AveragedPerceptronTagger(order=2)    
-    tagger.fit(X_train, y_train, epochs=20)
-    accuracy,w = tagger.evaluate(X_test, y_test)
-    trs = ["t_i-2='0',t_i-1='0'","t_i-2='0',t_i-1='1'","t_i-2='0',t_i-1='2'",\
-           "t_i-2='1',t_i-1='0'","t_i-2='1',t_i-1='1'","t_i-2='1',t_i-1='2'",\
-           "t_i-2='2',t_i-1='0'","t_i-2='2',t_i-1='1'","t_i-2='2',t_i-1='2'"]
+if __name__ == "__main__":
+
+    X_train, y_train, X_test, y_test = data_prep()
+    trans = MaximumLikelihoodNGramModel(y_train, 3).prob.items()  # transitions
+
+    tagger = AveragedPerceptronTagger(order=2)
+    tagger.fit(X_train, y_train, epochs=1)
+    accuracy, w = tagger.evaluate(X_test, y_test)
+    trs = ["t_i-2='0',t_i-1='0'", "t_i-2='0',t_i-1='1'", "t_i-2='0',t_i-1='2'",
+           "t_i-2='1',t_i-1='0'", "t_i-2='1',t_i-1='1'", "t_i-2='1',t_i-1='2'",
+           "t_i-2='2',t_i-1='0'", "t_i-2='2',t_i-1='1'", "t_i-2='2',t_i-1='2'"]
     for tr in trs:
         print tr
         print w[tr]
