c976f96659c29c16bf9146d9d1ad844afb59508d
Shiran <dudy@ohsu.edu>
2015-10-22 23:20:26 -0700

    licesne

--- mp4/sec_ord_vit.py
+++ mp4/sec_ord_vit.py
@@ -1,3 +1,21 @@
+#! /opt/local/bin/python -O
+# MP4 skeleton implementation
+# Kyle Gorman <gormanky@ohsu.edu>
+# taken from NLP class at OHSU from http://www.cslu.ogi.edu/~gormanky/courses/CS662/
+# this code was modifed by Shiran Dudy <dudy@ohsu.edu>
+
+#Redistribution and use in source and binary forms are permitted
+#provided that the above copyright notice and this paragraph are
+#duplicated in all such forms and that any documentation,
+#advertising materials, and other materials related to such
+#distribution and use acknowledge that the software was developed
+#by the CSLU. The name of the
+#CSLU may not be used to endorse or promote products derived
+#from this software without specific prior written permission.
+#THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+#IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 #/usr/bin/python
 # -*- coding:utf-8 -*-
 states = ('Rainy','Sunny')
